e3311fb30ae36b05a957401a7cb97b4d4498866c
liuxulelouch <liuxu-zero.1210@163.com>
2014-11-22 15:55:47 +0800

    添加了一个类

---
+++ Github/SecondViewController.h
@@ -0,0 +1,13 @@
+//
+//  SecondViewController.h
+//  Github
+//
+//  Created by zero on 14-11-22.
+//  Copyright (c) 2014年 lelouch. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SecondViewController : UIViewController
+
+@end
